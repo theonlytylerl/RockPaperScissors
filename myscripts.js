1132c0387d1cea22076167a5aa6cf4c77c527f3c
theonlytylerl <107710735+theonlytylerl@users.noreply.github.com>
2024-12-13 00:59:46 -0600

    Finished project! Learned that functions are not arrays, and need to define parameters into an array if wanting to use them in iterative loop.

--- myscripts.js
+++ myscripts.js
@@ -25,4 +25,59 @@ function getHumanChoice() {
     }
 }
 
-console.log(getHumanChoice())
+
+
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
+    let [x, y] = [humanScore, computerScore]
+    
+    function playRound(humanChoice, computerChoice) {
+        let human = 0;
+        let computer = 0;
+        let choices = [humanChoice, computerChoice]
+        for (let i = 0; i < 2; i++) {
+            if (choices[i] === "Rock") {
+                if (i === 0) {
+                    human = 0;
+                } else {
+                    computer = 0;
+                }
+            } else if (choices[i] === "Paper") {
+                if (i === 0) {
+                    human = 1;
+                } else {
+                    computer = 1;
+                }
+            } else {
+                if (i === 0) {
+                    human = 2;
+                } else {
+                    computer = 2;
+                }
+            }
+        }
+    
+        let result = (computer - human) % 3;
+        console.log("Your pick: " + humanChoice + "      Computer pick: " + computerChoice)
+        if (result === -1 || result === 2) {
+            console.log("You win!")
+            humanScore += 1
+        } else if (result === 1 || result === -2) {
+            console.log("You lose :(")
+            computerScore += 1
+        } else {
+            console.log("It's a tie!")
+        }
+        return [humanScore, computerScore]
+    }
+
+    for (let k = 0; k < 5; k++) {
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        [x, y] = playRound(humanSelection, computerSelection)
+    }
+    return ("You: " + x + "    Computer: " + y)
+}
+
+console.log(playGame())
