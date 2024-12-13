25a3922e4be6a86dc32f068c7550d357b67a949c
theonlytylerl <107710735+theonlytylerl@users.noreply.github.com>
2024-12-12 19:18:25 -0600

    Add code to prompt user for their choice of rock, paper, or scissors.

--- myscripts.js
+++ myscripts.js
@@ -12,4 +12,17 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    while (true) {
+        let input = prompt("Please pick 'Rock,' 'Paper,' or 'Scissors'")
+        let choice = input.charAt(0).toUpperCase() + input.slice(1)
+        if (choice == "Rock" || choice == "Paper" || choice == "Scissors") {
+         return choice
+         break;
+        } else {
+            console.log("Please enter a valid choice")
+        }
+    }
+}
+
+console.log(getHumanChoice())
