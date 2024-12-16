03f3114857124e961a589f3c0d2c83d26a94ef98
theonlytylerl <107710735+theonlytylerl@users.noreply.github.com>
2024-12-16 16:07:51 -0600

    Add UI for rock paper scissors.

--- myscripts.js
+++ myscripts.js
@@ -31,6 +31,7 @@ function playGame() {
     let humanScore = 0
     let computerScore = 0
     let [x, y] = [humanScore, computerScore]
+    let roundCount = 0
     
     function playRound(humanChoice, computerChoice) {
         let human = 0;
@@ -59,25 +60,71 @@ function playGame() {
         }
     
         let result = (computer - human) % 3;
-        console.log("Your pick: " + humanChoice + "      Computer pick: " + computerChoice)
-        if (result === -1 || result === 2) {
-            console.log("You win!")
-            humanScore += 1
-        } else if (result === 1 || result === -2) {
-            console.log("You lose :(")
-            computerScore += 1
-        } else {
-            console.log("It's a tie!")
+
+        const results = document.querySelector(".results");
+        const roundResults = document.querySelector(".roundResults")
+        roundResults.innerText = `You picked ${humanChoice} and they picked ${computerChoice}.`
+
+        if (roundCount > 0) {
+            results.removeChild(results.children[0])
         }
+
+        if (result === -1 || result === 2) {
+            const whoWon = document.createElement("span");
+            whoWon.textContent = "You win!"
+            results.appendChild(whoWon)
+            humanScore += 1;
+        } else if (result === 1 || result === -2) {
+            const whoWon = document.createElement("span");
+            whoWon.textContent = "You lose :("
+            results.appendChild(whoWon)
+            computerScore += 1;
+        } else {
+            const whoWon = document.createElement("span");
+            whoWon.textContent = "It's a tie. Wow!"
+            results.appendChild(whoWon)
+            console.log("It's a tie!");
+        }
+
+        const roundScore = document.querySelector(".score")
+        roundScore.textContent = `You: ${humanScore} Computer: ${computerScore}`
+        roundCount += 1
+
+        if (humanScore == 5 || computerScore == 5) {
+            for (let i = 0; i < button.length; i++){
+                button[i].disabled = "true";
+            }
+            
+            const victory = document.createElement("div");
+            if (humanScore == 5) {
+                victory.textContent = "You are crowned the victor of Rock Paper Scissors!"
+            } else {
+                victory.textContent = "You let the computer become king :( maybe try again another day."
+            }
+            roundScore.appendChild(victory)
+        }
+
         return [humanScore, computerScore]
     }
 
-    for (let k = 0; k < 5; k++) {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        [x, y] = playRound(humanSelection, computerSelection)
+    const button = document.querySelectorAll("button");
+        for (let i = 0; i < button.length; i++){
+        button[i].addEventListener('click', () => {
+            let computerSelection = getComputerChoice();
+            let humanSelection = button[i].textContent;
+            
+            return playRound(humanSelection, computerSelection);
+        })
     }
+
+    // Controls the amount of rounds played in a game, k < "how many rounds"
+    // for (let k = 0; k < 1; k++) {
+    //     let humanSelection = getHumanChoice();
+    //     let computerSelection = getComputerChoice();
+    //     [x, y] = playRound(humanSelection, computerSelection)
+    // }
+
     return ("You: " + x + "    Computer: " + y)
 }
 
-console.log(playGame())
+playGame()
